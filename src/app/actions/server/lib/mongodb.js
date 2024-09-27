f16eac8da59bfa4c632c5fee34dafe2609a193ae
bilalrana351 <bilalrana351@gmail.com>
2024-09-27 15:30:03 +0500

    Resolved the database issue

--- src/app/actions/server/lib/mongodb.js
+++ src/app/actions/server/lib/mongodb.js
@@ -16,13 +16,14 @@ const dbConnect = async () => {
     const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: 'convertly' // Explicitly specify the database name
     });
     isConnected = db.connections[0].readyState; // 1 means connected
 
-    console.log('Database connected successfully'); // Remove or replace in production
+    console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
-    process.exit(1); // Optional: Exit the process if the initial connection fails
+    process.exit(1);
   }
 };
 
